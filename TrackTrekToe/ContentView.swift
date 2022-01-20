a79730ecb1262b8200fe2c1f6250db67af46427d
Baris Karalar <karalarbaris@gmail.com>
2022-01-20 10:57:57 +0100

    Initial Commit

---
+++ TrackTrekToe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TrackTrekToe
+//
+//  Created by Baris Karalar on 20.01.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TrackTrekToe/TrackTrekToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackTrekToeApp.swift
+//  TrackTrekToe
+//
+//  Created by Baris Karalar on 20.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrackTrekToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
